cbbcf3d9a64dc3222a80fd45b3f1b9e0b55586fc
sophiayj <okyjok@163.com>
2016-10-23 20:30:57 +0800

    add code

---
+++ lab6/src/TestLab6.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Method;
+
+/**
+ * Instructions:
+ * You can run this file to check whether you have solved the problems correctly.
+ * You MUST NOT MODIFY this file.
+ *
+ * Created by Jun Yuan on 23/10/2016.
+ */
+public class TestLab6 {
+    public static void main(String[] argv) {
+        try {
+            Class lab6;
+            lab6 = Class.forName("Lab6");
+
+            char[] a = {'1', '2', '3', '4', '5', '6'};
+            char[] b = {'1', '2', '2', '4', '5', '6'};
+            char[] c = {'6', '6', '6'};
+
+            Method m = lab6.getMethod("compare", new Class[]{char[].class, char[].class});
+            if ((boolean) m.invoke(lab6, a, b) && (boolean) m.invoke(lab6, b, c) && !(boolean) m.invoke(lab6, c, a))
+                System.out.println("Congratulations! You solved problem 1.");
+            else
+                System.out.println("Oops! Something wrong with your problem 1.");
+
+
+            int[][] aa = {{3, 2, 3}, {2, 2, 2}, {1, 2, 2}};
+            int[][] bb = {{1, 2, 3, 3}, {2, 2, 2, 2}};
+            int[][] cc = {{1, 2, 3, 4, 4, 3, 2, 1}};
+
+            m = lab6.getMethod("compare", new Class[]{int[][].class, int[][].class});
+            if ((boolean) m.invoke(lab6, aa, bb) && !(boolean) m.invoke(lab6, aa, cc) && !(boolean) m.invoke(lab6, bb, cc))
+                System.out.println("Congratulations! You solved problem 2.");
+            else
+                System.out.println("Oops! Something wrong with your problem 2.");
+
+        } catch (Exception e) {
+            System.out.println("Oops! Something wrong with your code.");
+            e.printStackTrace();
+        }
+    }
+}
