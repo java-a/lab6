086a3a5e5c28903acb26bcdb530bdac04f8cf251
sophiayj <okyjok@163.com>
2016-10-23 20:19:48 +0800

    add code

---
+++ lab6/src/Lab6.java
@@ -0,0 +1,37 @@
+/**
+ * Instruction:
+ * You should write your code in this file to pass the test.
+ * Please feel free to contact you TAs if you have any questions :)
+ *
+ * Created by Jun Yuan on 23/10/2016.
+ */
+public class Lab6 {
+
+    /** Returns the result of comparing 2 big numbers.
+     *  Returns true if the numbers are equal.
+     *  @param a    a char array consists of '0'..'9'
+     *  @param b    a char array consists of '0'..'9'
+     * */
+    public static boolean compare(char[] a, char[] b) {
+        // your code here.
+
+        return false;
+    }
+
+    /** Returns the result of comparing the sum of 2 integer arrays.
+     *  Returns true if the 2 sums are equal.
+     *  @param a    an integer array represents the size of a hotel a.
+     *  @param b    an integer array represents the size of a hotel b.
+     * */
+    public static boolean compare(int[][] a, int[][] b) {
+        int sum_of_a = 0;
+        int sum_of_b = 0;
+
+        int row_of_a = a.length;
+        int col_of_a = a[0].length;
+
+        // your code here.
+
+        return false;
+    }
+}
